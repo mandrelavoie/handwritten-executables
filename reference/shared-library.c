781d9de3335430fd23a3af26c5808cc04f0cef60
Marc-André Lavoie <m.andre.lavoie@gmail.com>
2020-11-13 17:58:11 -0500

    Created a minimal c shared library for reference

---
+++ reference/shared-library.c
@@ -0,0 +1,12 @@
+// gcc -shared -Os -s -nostdlib -o shared-library.so shared-library.c
+
+char* message = "Hello from a shared library!\n";
+
+extern void say_hello(void) {
+	__asm__("movq $1, %%rax \n\t"
+		"movq $1, %%rdi \n\t"
+		"movq %0, %%rsi \n\t"
+		"movl %1, %%edx \n\t"
+		"syscall"
+		:: "g"(message), "g" (29));
+}
